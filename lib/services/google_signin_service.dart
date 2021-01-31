940a4f985783f410355badbd15e1ad6c66f30031
julian <juliandanielfranco@gmail.com>
2021-01-31 09:28:17 -0500

    feat: improve singup with google validation backend

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSigninService {
   static GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -11,20 +12,22 @@ class GoogleSigninService {
     try {
       final GoogleSignInAccount account = await _googleSignIn.signIn();
       final googleKey = await account.authentication;
-      print(account);
-
-      print("======= ID TOKEN ======");
-
-
-      print(googleKey.idToken);
 
       // todo: llamar servicio al backed con el idtoken
 
+      final signInWithGoogleEndpoint = Uri(
+          scheme: "https",
+          host: "flutter-google-signin-apple.herokuapp.com",
+          path: "/google");
+
+      final session = await http
+          .post(signInWithGoogleEndpoint, body: {'token': googleKey.idToken});
+      
+      print(session.body);
 
 
 
       return account;
-
     } catch (e) {
       print(e);
       return null;
